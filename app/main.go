4ec37f3638720c88e73d696de99966a28c59f224
wurzeit <hiccap.master.jedi@gmail.com>
2023-01-22 17:21:27 +0900

    add http server

--- app/main.go
+++ app/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "home")
+    fmt.Println("Endpoint Hit: home")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", home)
+    log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Println("Server started")
+    handleRequests()
 }

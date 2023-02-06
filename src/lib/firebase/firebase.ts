89822c75f8db017163045d8c8e07473923956a0e
Yanai1005 <tatsuya871_1005@icloud.com>
2023-02-06 19:08:16 +0900

    Revert "[edit]sign's page"
    This reverts commit 824903c151c7967e4ea80b1bc94f6ebc3ff42bf7.

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,5 +1,7 @@
+// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from 'firebase/app'
-
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 import {
     FIREBASE_API_KEY,
     FIREBASE_APP_ID,
@@ -9,6 +11,7 @@ import {
     FIREBASE_STORAGE_BUCKET,
 } from 'constant/env'
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: FIREBASE_API_KEY,
     authDomain: FIREBASE_AUTH_DOMAIN,
@@ -18,6 +21,7 @@ const firebaseConfig = {
     appId: FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const initializeFirebaseApp = () =>
     !getApps().length ? initializeApp(firebaseConfig) : getApp()

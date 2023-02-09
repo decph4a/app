78709e54ac9d5b59bd5aeab0f0fb00824fc46a32
Yanai1005 <tatsuya871_1005@icloud.com>
2023-02-09 19:00:44 +0900

    [edit]fixed footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { chakra, ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from "next/app";
 import { initializeFirebaseApp } from '../lib/firebase/firebase'
 import { AuthProvider } from '../feature/auth/provider/AuthProvider'
 import { getApp } from 'firebase/app'
 import { Header } from '../component/Header/Header'
 import { Footer } from '../component/Footer/Footer'
+import { theme } from 'lib/chakra/theme'
 
 
 initializeFirebaseApp()
 export default function App({ Component, pageProps }: AppProps) {
     console.log(getApp())
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <AuthProvider>
                 <Header />
-                <Component {...pageProps} />
+                <chakra.main flex={1} display={'flex'} flexDirection={'column'}>
+                    <Component {...pageProps} />
+                </chakra.main>
                 <Footer />
             </AuthProvider>
         </ChakraProvider>

7eb8511f69c75d4d832e4ad915c44260fd54e8c5
Yanai1005 <tatsuya871_1005@icloud.com>
2023-02-13 18:25:47 +0900

    [edit]change setting

---
+++ src/lib/pathpida/$path.ts
@@ -0,0 +1,17 @@
+export const pagesPath = {
+  "chat": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/chat' as const, hash: url?.hash })
+  },
+  "dashboard": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/dashboard' as const, hash: url?.hash })
+  },
+  "signin": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/signin' as const, hash: url?.hash })
+  },
+  "signup": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/signup' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+export type PagesPath = typeof pagesPath

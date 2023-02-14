2352b77767acab74fc670617a882caf55f7251b6
Yanai1005 <tatsuya871_1005@icloud.com>
2023-02-14 19:46:58 +0900

    [add]sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,23 +1,53 @@
+import React from 'react'
 import { Flex, Text } from "@chakra-ui/layout"
 import { Button } from "@chakra-ui/react"
 import { IconButton } from "@chakra-ui/button"
 import { Avatar } from "@chakra-ui/avatar"
 import { ArrowLeftIcon } from "@chakra-ui/icons"
 import { signOut } from 'firebase/auth'
-import { auth } from 'lib/firebase/firebase'
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from 'lib/firebase/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { collection } from "firebase/firestore"
+import getOtherEmail from 'utils/getOtherEmail'
 
-const Chat = () => {
-    return (
-        < Flex p={3} align="center" _hover={{ bg: "gray.100", cursor: "pointer" }}>
-            <Avatar src="" marginEnd={3} />
-            <Text>hogehoge</Text>
-        </Flex >
-    )
+interface Chat {
+    id: string;
+    users: string[];
+    [key: string]: any;
 }
-export default function Sidebar() {
+interface User {
+    email: string | null;
+}
+function displayEmail(user: User | null | undefined) {
+    if (user && user.email != null) {
+        console.log(user.email);
+    } else {
+        console.log("Email not available");
+    }
+}
 
-    const [user] = useAuthState(auth);
+const Sidebar: React.FC = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const [snapshot, chatsLoading, chatsError] = useCollection(collection(db, "chats"));
+    const chats = snapshot?.docs.map(doc => ({ id: doc.id, ...doc.data() })) as Chat[];
+    const email = user?.email || '';
+    const chatlist = () => {
+        return (
+            chats?.filter(chat => chat.users.includes(user?.email || ''))
+                .map(
+                    (chat) => (
+                        <Flex key={Math.random()} p={3} align="center" _hover={{ bg: "gray.100", cursor: "pointer" }}>
+                            <Avatar src="" marginEnd={3} />
+                            <Text>
+                                {user && user.email ? getOtherEmail(chat.users, user) : 'Email not found'}
+                            </Text>
+                        </Flex>
+                    )
+                )
+        );
+    };
+
     return (
         <Flex
             h="100%"
@@ -35,18 +65,18 @@ export default function Sidebar() {
                     align="center"
                 >
                     <Avatar src={user?.photoURL || ''} marginEnd={3} />
-                    <Text>{user && user.displayName}</Text>
-
+                    <Text>{user?.displayName}</Text>
                 </Flex>
 
-                <IconButton aria-label='Search database' size="sm" isRound icon={<ArrowLeftIcon />} onClick={() => signOut(auth)} />
+                <IconButton aria-label='Sign out' size="sm" isRound icon={<ArrowLeftIcon />} onClick={() => signOut(auth)} />
             </Flex>
 
             <Button m={5} p={4}>New Chat</Button>
             <Flex overflowX="scroll" direction="column" sx={{ scrollbarwidth: "none" }} flex={1}>
-                <Chat />
+                {chatlist()}
             </Flex>
+        </Flex>
+    );
+};
 
-        </Flex >
-    )
-}
+export default Sidebar;

75e6ae202d5a4b506d91d8af785e8f0403781a0e
Yanai1005 <tatsuya871_1005@icloud.com>
2023-02-16 17:02:58 +0900

    [remove]unnecessary

--- src/lib/pathpida/$path.ts
+++ src/lib/pathpida/$path.ts
@@ -8,12 +8,6 @@ export const pagesPath = {
   "dashboard": {
     $url: (url?: { hash?: string }) => ({ pathname: '/dashboard' as const, hash: url?.hash })
   },
-  "signin": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/signin' as const, hash: url?.hash })
-  },
-  "signup": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/signup' as const, hash: url?.hash })
-  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
-// import { initializeFirebaseApp } from '../lib/firebase/firebase'
-import { chakra, ChakraProvider, Spinner, Center } from '@chakra-ui/react'
+
+import { ChakraProvider, Spinner, Center } from '@chakra-ui/react'
 import { AppProps } from "next/app";
-import { AuthProvider } from '../feature/auth/provider/AuthProvider'
-import { getApp } from 'firebase/app'
-import { theme } from 'lib/chakra/theme'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from "lib/firebase/firebase"
 import Login from 'components/Login'
 
 
-// initializeFirebaseApp()
 function App({ Component, pageProps }: AppProps) {
     const [user, loading, error] = useAuthState(auth);
     if (loading) {
